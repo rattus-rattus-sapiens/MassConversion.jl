6e6b1497227e7cf31e47993451fa4be1ac7ae8d3
Joshua C. Kynaston <josh.c.kynaston@gmail.com>
2022-03-17 13:12:52 +0000

    sandbox: add experimental code for model autobuilding

--- src/MassConversion.jl
+++ src/MassConversion.jl
@@ -1,5 +1,13 @@
 module MassConversion
 
+#using OrderedCollections
+
 include("mcm_functions.jl")
+#include("model_setup.jl")
+
+export run_mcm
+
+#export Model, Species, Reaction
+#export get_init
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,18 @@ using Test
 
 @testset "MassConversion.jl" begin
     # Write your tests here.
+#   @testset "Model setup tests" begin
+#       model = Model()
+#       model <= Species("A", 100)
+#       model <= Species("B", 10)
+#       model <= Species("EE", 9)
+#
+#       species = get_init(model)
+#       @test species["A"] == 100
+#       @test species["B"] == 10
+#       @test species["EE"] == 9
+#       @test_throws MassConversion.ModelSpecError Species("A+A", 0)
+#       @test_throws MassConversion.ModelSpecError Species("A + 33", 4)
+#       @test_throws MassConversion.ModelSpecError model <= Species("A", 10)
+#    end
 end
