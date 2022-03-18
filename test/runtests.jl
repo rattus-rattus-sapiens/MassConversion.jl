14e2d089fccf7bcbf158896b4464cc836a390fba
Joshua C. Kynaston <josh.c.kynaston@gmail.com>
2022-03-18 01:36:00 +0000

    test: add test for data_structures
    code coverage!

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,38 @@ using Test
 
 @testset "MassConversion.jl" begin
     # Write your tests here.
+
+    @testset "Data Structure Tests" begin
+        traj1 = ones(10, 1000, 250)
+
+        mn = MassConversion.calc_mean(traj1)
+        @test size(mn) == (10, 1000)
+        @test all(mn .≈ 1)
+        
+        mn2 = MassConversion.calc_mean_total(mn, mn)
+        @test size(mn2) == (10, 1000)
+        @test all(mn2 .≈ 1)
+
+        vr = MassConversion.calc_var(traj1, mn)
+        @test size(vr) == (10, 1000)
+        @test all(vr .≈ 0)
+
+        model = MassConversion.MCMOutput(traj1)
+        @test size(model.trajectories_continuum) == (5, 1000, 250)
+        @test size(model.trajectories_discrete)  == (5, 1000, 250)
+        @test size(model.mean_continuum)         == (5, 1000)
+        @test size(model.mean_discrete)          == (5, 1000)
+        @test size(model.mean_total)             == (5, 1000)
+        @test size(model.var_total)              == (5, 1000)
+
+        @test all(model.trajectories_continuum .== 1.0)
+        @test all(model.trajectories_discrete .== 1.0)
+        @test all(model.mean_continuum .== 1.0)
+        @test all(model.mean_discrete .== 1.0)
+        @test all(model.mean_total .== 1.0)
+    end
+end
+
 #   @testset "Model setup tests" begin
 #       model = Model()
 #       model <= Species("A", 100)
@@ -17,4 +49,3 @@ using Test
 #       @test_throws MassConversion.ModelSpecError Species("A + 33", 4)
 #       @test_throws MassConversion.ModelSpecError model <= Species("A", 10)
 #    end
-end
