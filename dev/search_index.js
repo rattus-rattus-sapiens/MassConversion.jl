ff5596f3278cdfe06102a9ce89ef634f65f6285a
Documenter.jl <documenter@juliadocs.github.io>
2022-03-18 00:48:10 +0000

    build based on db727be

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Home","page":"Index","title":"Home","text":"","category":"section"},{"location":"","page":"Index","title":"Index","text":"This package provides an implementation of the Mass Conversion hybrid method for the simulation of well-mixed chemical reaction networks.","category":"page"},{"location":"","page":"Index","title":"Index","text":"run_mcm()","category":"page"}]
+[{"location":"#Home","page":"Index","title":"Home","text":"","category":"section"},{"location":"","page":"Index","title":"Index","text":"This package provides an implementation of the Mass Conversion hybrid method for the simulation of well-mixed chemical reaction networks.","category":"page"},{"location":"","page":"Index","title":"Index","text":"run_mcm()\nrun_ssa()","category":"page"}]
 }
