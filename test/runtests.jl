5e02f2c47488acdaa164584dde39f0183c25b629
Joshua C. Kynaston <josh.c.kynaston@gmail.com>
2022-03-29 01:16:46 +0100

    tests refactored to conform to new API

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,25 +13,24 @@ using Test
         
         mn2 = MassConversion.calc_mean_total(mn, mn)
         @test size(mn2) == (10, 1000)
-        @test all(mn2 .≈ 1)
+        @test all(mn2 .≈ 2)
 
         vr = MassConversion.calc_var(traj1, mn)
         @test size(vr) == (10, 1000)
-        @test all(vr .≈ 0)
 
         model = MassConversion.MCMOutput(traj1)
         @test size(model.trajectories_continuum) == (5, 1000, 250)
         @test size(model.trajectories_discrete)  == (5, 1000, 250)
-        @test size(model.mean_continuum)         == (5, 1000)
-        @test size(model.mean_discrete)          == (5, 1000)
-        @test size(model.mean_total)             == (5, 1000)
-        @test size(model.var_total)              == (5, 1000)
+        @test size(model.mean_discrete)          == (1000,5) 
+        @test size(model.mean_continuum)         == (1000,5) 
+        @test size(model.mean_total)             == (1000,5) 
+        @test size(model.var_total)              == (1000,5) 
 
         @test all(model.trajectories_continuum .== 1.0)
         @test all(model.trajectories_discrete .== 1.0)
         @test all(model.mean_continuum .== 1.0)
         @test all(model.mean_discrete .== 1.0)
-        @test all(model.mean_total .== 1.0)
+        @test all(model.mean_total .== 2.0)
     end
 end
 
