842862d070c3614008b695ffc0e6503c65830332
Joshua C. Kynaston <josh.c.kynaston@gmail.com>
2022-03-30 23:20:54 +0100

    chore: whitespace

--- test/runtests.jl
+++ test/runtests.jl
@@ -60,18 +60,3 @@ using Test
         @test alpha[10] == 50.0
     end
 end
-
-#   @testset "Model setup tests" begin
-#       model = Model()
-#       model <= Species("A", 100)
-#       model <= Species("B", 10)
-#       model <= Species("EE", 9)
-#
-#       species = get_init(model)
-#       @test species["A"] == 100
-#       @test species["B"] == 10
-#       @test species["EE"] == 9
-#       @test_throws MassConversion.ModelSpecError Species("A+A", 0)
-#       @test_throws MassConversion.ModelSpecError Species("A + 33", 4)
-#       @test_throws MassConversion.ModelSpecError model <= Species("A", 10)
-#    end
