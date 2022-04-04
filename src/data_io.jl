fdd7b34d1defb4142dffbfa35bcc5e4d7e4ab1f1
Joshua C. Kynaston <josh.c.kynaston@gmail.com>
2022-04-04 17:49:40 +0100

    actually just make IO keys the same

--- src/data_io.jl
+++ src/data_io.jl
@@ -1,23 +1,28 @@
-function save_data(rec, casename::String)
+function save_data(data::Any)
+    filename = string(floor(now(), Dates.second)) * ".jld2"
+    jldsave(filename, data)
+end
+
+function save_data(data, casename::String)
     filename = "examples/" * casename * string(floor(now(), Dates.Second)) * ".jld2"
-    jldsave(filename; rec)
+    jldsave(filename; data)
     println("Saved at " * filename)
 end
 
-function save_data(rec, casename::String, filename::String)
+function save_data(data, casename::String, filename::String)
     filename = "examples/" * casename  * "/dat/" * filename * ".jld2"
-    jldsave(filename; rec)
+    jldsave(filename; data)
     println("Saved at " * filename)
 end
 
 function load_data(casename::String, filename::String)
     if filename[end-4 : end] == ".jld2"
         jldopen("examples/" * casename * "/dat/" * filename) do file
-            return Dict(key => file[key] for key in keys(file)) 
+            return file["data"]
         end
     else
         jldopen("examples/" * casename * "/dat/" * filename * ".jld2", "r") do file
-            return Dict(key => file[key] for key in keys(file))
+            return file["data"]
         end
     end
 end
