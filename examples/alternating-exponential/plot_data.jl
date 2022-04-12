3d1290cb5c6332bf326cea6b33c9668fa124117c
Joshua C. Kynaston <josh.c.kynaston@gmail.com>
2022-04-12 19:11:44 +0100

    update plotting code for alt-exp example

--- examples/alternating-exponential/plot_data.jl
+++ examples/alternating-exponential/plot_data.jl
@@ -2,16 +2,13 @@ using Revise
 using Plots
 using MassConversion
 
-a, b = load_raw("/home/jkynaston/git/MassConversion.jl/examples/alternating-exponential/dat/Tue-12-00:05:36")
+a, b = load_raw("/home/jkynaston/git/MassConversion.jl/examples/alternating-exponential/dat/Tue-12-19:08:09")
 
 data_mcm_full = sum(a)
 data_ssa_full = sum(b)
 
-plot(data_mcm_full.D)
-plot!(data_mcm_full.C)
-plot!(data_mcm_full.D .+ data_mcm_full.C)
-
-plot!(data_ssa_full.D)
+p = plot(data_mcm_full)
+plot(p, data_ssa_full)
 
 gt = data_ssa_full.D
 pt = data_mcm_full.D .+ data_mcm_full.C 
