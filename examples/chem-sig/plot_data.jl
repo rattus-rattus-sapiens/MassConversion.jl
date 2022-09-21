55d8e81f630acee8b6a221e78457d4da2c6940d0
Joshua C. Kynaston <josh.c.kynaston@gmail.com>
2022-09-21 15:48:54 +0100

    change plot code

--- examples/chem-sig/plot_data.jl
+++ examples/chem-sig/plot_data.jl
@@ -3,6 +3,8 @@ using Plots
 using MassConversion
 using LaTeXStrings
 using DifferentialEquations
+using DataFrames
+using CSV
 pgfplotsx();
 
 # --------- SOLV ODES ---------- #
@@ -27,6 +29,17 @@ mcm = load_raw(joinpath("/home/jkynaston/git/MassConversion.jl/examples/chem-sig
 ssa_full = sum(ssa);
 mcm_full = sum(mcm);
 
+df = DataFrame()
+
+df.MCM = MCM = mcm_full.C[1:50:end,2] + mcm_full.D[1:50:end,2]
+df.SSA = SSA = ssa_full.D[1:50:end,2]
+df.ODE = ODE = sol(0:5:1000, idxs=2)
+df.X = 0:1:200
+df.ERRX1 = (mcm_full.C[1:50:end,1] + mcm_full.D[1:50:end,1] - ssa_full.D[1:50:end,1])./ssa_full.D[1:50:end,1]
+df.ERRX2 = (MCM - SSA)./SSA
+df.ERRX3 = (mcm_full.C[1:50:end,3] + mcm_full.D[1:50:end,3] - ssa_full.D[1:50:end,3])./ssa_full.D[1:50:end,3]
+CSV.write("data.csv", df)
+
 p = plot(dpi=300,
         size=(400,300),
         xtickfontsize=9,
@@ -36,11 +49,15 @@ p = plot(dpi=300,
         ylims=(0,310),
         legend=:bottomright
     );
-plot!(p, mcm_full.C[1:50:end,2]+mcm_full.D[1:50:end,2],linewidth=2,label=L"X_1");
-plot!(p, ssa_full.D[1:50:end,2],lw=2,label=L"X_2");
-plot!(p, sol(0:5:1000, idxs=2), lw=2,label=L"X_3")
 
+MCM = mcm_full.C[1:50:end,2] + mcm_full.D[1:50:end,2]
+SSA = ssa_full.D[1:50:end,2]
 
+plot!(p, mcm_full.C[1:50:end,2]+mcm_full.D[1:50:end,2],linewidth=2,label="MCM");
+plot!(p, ssa_full.D[1:50:end,2],lw=2,label="SSA");
+plot!(p, sol(0:5:1000, idxs=2), lw=2,label="ODE")
+
+plot(p, (MCM - SSA)./SSA)
 
 p = plot(dpi=300);
 plot(p, err_full)
